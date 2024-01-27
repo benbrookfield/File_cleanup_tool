83ca908d83833917e3a810aaf6c42a81913222ab
BenBrookfield <Benbrookyboy@gmail.com>
2024-01-27 23:16:52 +0000

    Implemented cleanup function.

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import os
 from fileclass import File
+import shutil
 
-def cleanup():
-    return
 
-def stop_files_moving():
+def cleanup(files):
+    print("hello")
+    for key, values in files.items():
+        if values[1]:
+            try:
+                shutil.move(r"C:\Users\munki\Downloads\\" + key, values[0])
+            except:
+                print("There was an error moving {}, check file paths are correct and that the file does not already exist in the destination.".format(key))
+
+    print("")
+    print("Files have been moved, cleanup finished.")
+
+
+def stop_files_moving(files):
+    print(" ")
+    fname = input("""Enter file name:
+""")
+
     try:
-        fname = input()
-        object(fname).set_move_false()
+        files[fname][1] = False
         print(fname, 'Will stay in its original folder, if there are any other files you do not want to move, enter "Y"')
+        for key, values in files.items():
+            print(key, "--->", values[0], values[1])
         response = input()
         if response == "Y":
-            print("Enter file name:")
-            stop_files_moving()
+            #print("Enter file name:")
+            stop_files_moving(files)
 
     except:
         print('That is not a valid file name, please enter the exact file name or enter "EXIT" to commence cleanup.')
@@ -20,10 +37,11 @@ def stop_files_moving():
         if answer == "EXIT":
             return
         else:
-            stop_files_moving()
+            stop_files_moving(files)
 
 
-#with os.scandir(r"C:\Users\munki\Documents\cleanup test") as entries:
+files = {}
+
 with os.scandir(r"C:\Users\munki\Downloads") as entries:
     for entry in entries:
         #print(entry.name)
@@ -31,34 +49,35 @@ with os.scandir(r"C:\Users\munki\Downloads") as entries:
 
         if entry.name.endswith(".mp3") or entry.name.endswith(".wav"):
             if entry.name.startswith("ElevenLabs"):
-                newdir = r"C:\Users\munki\Pictures\youtube\Voices for yt"
+                newdir = "C:/Users/munki/Pictures/youtube/Voices for yt"
             else:
-                newdir = r"C:\Users\munki\Music"
+                newdir = "C:/Users/munki/Music"
 
-        elif entry.name.endswith(".pdf" or ".docx" or ".doc" or ".txt"):
-            newdir = r"C:\Users\munki\Documents"
+        elif entry.name.endswith(".pdf") or entry.name.endswith(".docx") or entry.name.endswith(".doc") or entry.name.endswith(".txt"):
+            newdir = "C:/Users/munki/Documents"
 
         elif entry.name.endswith(".mp4"):
-            newdir = r"C:\Users\munki\Videos"
+            newdir = "C:/Users/munki/Videos"
 
-        elif entry.name.endswith(".jpg" or ".png" or ".gif" or ".eps" or ".jpeg" or ".svg" or ".bmp" or ".webp" or ".avif"):
-            newdir = r"C:\Users\munki\Pictures"
+        elif entry.name.endswith(".jpg") or entry.name.endswith(".png") or entry.name.endswith(".jpeg") or entry.name.endswith(".gif") or entry.name.endswith(".svg") or entry.name.endswith(".eps") or entry.name.endswith(".webp"):
+            newdir = r"C:/Users/munki/Pictures"
 
         else:
             move = False
             newdir = "Not being moved"
 
-        name = entry.name
-        name = File(newdir, move)
-        print(entry.name, "--->", name.get_newdir() + ",", name.get_move())
+        files[entry.name] = [newdir, move]
+
+#after all entries have been read, print dictionary showing files and where they will be moved to
+for key, values in files.items():
+  print(key, "--->", values[0])
 
 print()
 print('Are there any files you do not want to move? Enter "yes" if there are:')
 answer = input()
 
 if answer == "yes":
-    print("Enter file name:")
-    stop_files_moving()
+    stop_files_moving(files)
 
 else:
-    cleanup()
+    cleanup(files)
